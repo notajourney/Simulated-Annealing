4b40034a69a1f711dcb73939cba1df1583f7ee89
notajourney <33099467+notajourney@users.noreply.github.com>
2019-12-11 15:50:50 +0200

    notes for simulated annealing

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -7,24 +7,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import objFunctions as fct
 
-def SimulatedAnnealing(n, lb, ub, max_evals, variation=lambda x: x+np.random.normal(size=len(x)), func=lambda x: x.dot(x), seed=None) :
-    T_init=6.0
-    T_min=1e-4
-    alpha=0.99
-    f_lower_bound = 0
-    eps_satisfactory = 1e-5
-    max_internal_runs = 50
-    local_state = np.random.RandomState(seed)
+def SimulatedAnnealing(n, max_evals, variation=lambda x: x+np.random.normal(size=len(x)), func=lambda x: x.dot(x), seed=None) :
+    T_init=6.0 #initial temp
+    T_min=1e-4 #minimal temp
+    alpha=0.99 # the factor that shrinks the temp
+    f_lower_bound = 0#??
+    eps_satisfactory = 1e-5#??
+    max_internal_runs = 50 # for internal loop
+    local_state = np.random.RandomState(seed)#??
     history = []
-    xbest = xmin = local_state.uniform(size=n)*(ub - lb) + lb
-    fbest = fmin = func(xmin)
+    
+    
+    xbest = xmin = local_state.uniform(size=n)*(ub - lb) + lb# we need to randomize +-1 vector of dim=100
+    fbest = fmin = func(xmin)#stays the same
     eval_cntr = 1
     T = T_init
     history.append(fmin)
+    
+    
     while ((T > T_min) and eval_cntr < max_evals) :
         for _ in range(max_internal_runs) :
-            x = variation(xmin)
-            f_x = func(x)
+            x = variation(xmin)# variation is the step
+                                # we need to implement step in discrete space
+            f_x = func(x)#func = swidish :: here we evaluate the new vector
             eval_cntr += 1
             dE = f_x - fmin 
             if dE <= 0 or local_state.uniform(size=1) < np.exp(-dE/T) :
