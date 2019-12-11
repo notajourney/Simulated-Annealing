e6c27c7f59ac335005562956c9db38571535d79b
notajourney <33099467+notajourney@users.noreply.github.com>
2019-12-11 15:17:03 +0200

    notes for hill climber

--- basicHillClimber.py
+++ basicHillClimber.py
@@ -7,24 +7,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import objFunctions as fct
 
-def basicHillClimber(n, lb, ub, evals, func=lambda x: x.dot(x)) :
+def basicHillClimber(n, evals, func=lambda x: x.dot(x)) :
     history = []
-    xmin = np.random.uniform(size=n)*(ub - lb) + lb
-    fmin = func(xmin)
-    history.append(fmin)
+    #randomize vector  of +1 -1 sized n into --> xmin
+    fmin = func(xmin)# the first  is the minimal
+    
+    history.append(fmin)# adding to histoy of mininma
+    
     for _ in range(evals) :
-        x = xmin + np.random.normal(size=n)
-        f_x = func(x)
-        if f_x < fmin :
-            xmin = x
+        x = xmin + np.random.normal(size=n)## step: we need to imolement discrete step:
+        # generate a random between zero to n-1 and flip that coordinate(multiply by -1)
+        f_x = func(x)#evaluate the new vector
+        if f_x < fmin :#checking if the new vector is "better" then previous vector (we need to maximaize)
+            xmin = x# if better: x min = x
             fmin = f_x
         history.append(fmin)
     return xmin,fmin,history
 #
 if __name__ == "__main__" :
-    lb,ub = -5,5
-    n=2
-    evals=10**5
-    xmin,fmin,history = basicHillClimber(n,lb,ub,evals,fct.WildZumba)
-    plt.semilogy(history)
+    
+    n=100
+    evals=10**3#number of calls to objective
+    xmin,fmin,history = basicHillClimber(n,evals,fct.SwedishPump)
+    plt.semilogy(history)#plot (not interessting)
     print("minimal Zumba found is ", fmin," at location ", xmin)
